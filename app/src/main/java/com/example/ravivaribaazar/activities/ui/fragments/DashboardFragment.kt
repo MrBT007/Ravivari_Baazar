d0e26c82c0ad508e33fe574b661d600c57b2ba54
MrBT007 <bhuttushar2019@gmail.com>
2022-08-28 12:19:47 +0530

    Done to show items in dashboardlist
    done upto 14

--- app/src/main/java/com/example/ravivaribaazar/activities/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/ravivaribaazar/activities/ui/fragments/DashboardFragment.kt
@@ -3,16 +3,23 @@ package com.example.ravivaribaazar.activities.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.ravivaribaazar.R
 import com.example.ravivaribaazar.activities.ui.activities.DashboardActivity
+import com.example.ravivaribaazar.activities.ui.adapters.DashboardItemsListAdapter
+import com.example.ravivaribaazar.activities.ui.adapters.MyProductsListAdapter
 import com.example.ravivaribaazar.databinding.FragmentDashboardBinding
 import com.example.ravivaribaazar.firestore.FirestoreClass
 import com.example.ravivaribaazar.models.Product
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.activity_dashboard.view.*
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_products.*
 
 
 class DashboardFragment : BaseFragment() {
@@ -54,9 +61,20 @@ class DashboardFragment : BaseFragment() {
     fun successDashboardItemsList(dashboardItemsList:ArrayList<Product>)
     {
         hideProgressDialog()
-        for(i in dashboardItemsList)
+        if(dashboardItemsList.size>0)
         {
-            Log.i("Item Title",i.title)
+            rv_dashboard_items.visibility = View.VISIBLE
+            no_dashboard_items_found.visibility = View.GONE
+
+            rv_dashboard_items.layoutManager = StaggeredGridLayoutManager(2,LinearLayout.VERTICAL)
+            rv_dashboard_items.setHasFixedSize(true)
+            val dashboardAdapter = DashboardItemsListAdapter(requireActivity(),dashboardItemsList)
+            rv_dashboard_items.adapter = dashboardAdapter
+        }
+        else
+        {
+            rv_my_products_items.visibility = View.GONE
+            no_products_found.visibility = View.VISIBLE
         }
     }
 
